86d0e478a8e5173a0e668f251367d2cd10e90370
Pratima Gautam <astute.yard@gmail.com>
2020-03-27 15:21:24 +0545

    Updates

---
+++ precache-manifest.af336ab8d9d755f222ed2e42749c4f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baaa797bf62de2fa1cf7cf290b7a9a57",
+    "url": "/react-aws-s3-file-uploads/index.html"
+  },
+  {
+    "revision": "6cb735f9dbeb24911702",
+    "url": "/react-aws-s3-file-uploads/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d8b3876fb2c9e65f5a7e",
+    "url": "/react-aws-s3-file-uploads/static/js/2.8abdeab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-aws-s3-file-uploads/static/js/2.8abdeab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb735f9dbeb24911702",
+    "url": "/react-aws-s3-file-uploads/static/js/main.bba9401a.chunk.js"
+  },
+  {
+    "revision": "c1f188b4c11e625e8f06",
+    "url": "/react-aws-s3-file-uploads/static/js/runtime-main.09c31733.js"
+  }
+]);
